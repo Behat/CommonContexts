b82fd6e4dc0a47a347e92626a148b0407503acdb
Jakub Zalas <jakub@zalas.pl>
2011-11-30 21:32:00 +0000

    Added context with hooks to build and cleanup the database schema with Doctrine in Symfony.

---
+++ Behat/CommonContext/SymfonyDoctrineContext.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Behat\CommonContext;
+
+use Behat\BehatBundle\Context\BehatContext;
+use Behat\Behat\Event\ScenarioEvent;
+use Doctrine\ORM\Tools\SchemaTool;
+
+/**
+ * Provides hooks for building and cleaning up a database schema with Doctrine.
+ *
+ * While building the schema it takes all the entity metadata known to Doctrine.
+ *
+ * @author Jakub Zalas <jakub@zalas.pl>
+ */
+class SymfonyDoctrineContext extends BehatContext
+{
+    /**
+     * @param \Behat\Behat\Event\ScenarioEvent $event
+     *
+     * @BeforeScenario
+     *
+     * @return null
+     */
+    public function beforeScenario(ScenarioEvent $event)
+    {
+        $this->buildSchema();
+    }
+
+    /**
+     * @param \Behat\Behat\Event\ScenarioEvent $event
+     *
+     * @AfterScenario
+     *
+     * @return null
+     */
+    public function afterScenario(ScenarioEvent $event)
+    {
+        $this->getEntityManager()->clear();
+    }
+
+    /**
+     * @return null
+     */
+    protected function buildSchema()
+    {
+        $metadata = $this->getMetadata();
+
+        if (!empty($metadata)) {
+            $tool = new SchemaTool($this->getEntityManager());
+            $tool->dropSchema($metadata);
+            $tool->createSchema($metadata);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function getMetadata()
+    {
+        return $this->getEntityManager()->getMetadataFactory()->getAllMetadata();
+    }
+
+    /**
+     * @return \Doctrine\ORM\EntityManager
+     */
+    protected function getEntityManager()
+    {
+        return $this->getContainer()->get('doctrine.orm.entity_manager');
+    }
+}
