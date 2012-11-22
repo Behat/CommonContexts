93f8637647c8dd88b2ed1a3b5aa50ba8d1d39495
Miloslav Nenadál <nenadalm@gmail.com>
2012-11-22 21:06:26 +0100

    Repaired getting of token

--- Behat/CommonContexts/SymfonyMailerContext.php
+++ Behat/CommonContexts/SymfonyMailerContext.php
@@ -107,6 +107,7 @@ class SymfonyMailerContext extends RawMinkContext implements KernelAwareInterfac
                 throw new \RuntimeException('Debug-Token not found in response headers. Have you turned on the debug flag?');
             }
             $token = isset($headers['X-Debug-Token']) ? $headers['X-Debug-Token'] : $headers['x-debug-token'];
+            $token = end($token);
         }
 
         return $this->kernel->getContainer()->get('profiler')->loadProfile($token);
