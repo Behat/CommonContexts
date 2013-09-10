a125e121c7233293c94e8c18f07a5394bce2c195
Thomas Subera <ts@25th-floor.com>
2013-09-10 15:54:47 +0200

    implement header check steps

--- Behat/CommonContexts/WebApiContext.php
+++ Behat/CommonContexts/WebApiContext.php
@@ -177,7 +177,35 @@ class WebApiContext extends BehatContext
         assertNotRegExp('/'.preg_quote($text).'/', $this->browser->getLastResponse()->getContent());
     }
 
-    /**
+	/**
+	 * Checks that response Header contains specific text.
+	 *
+	 * @param string $header
+	 *
+	 * @param string $type
+	 *
+	 * @Then /^(?:the )?response header "([^"]*)" should contain "([^"]*)"$/
+	 */
+	public function theResponseHeaderShouldContain($header, $type)
+	{
+		assertRegExp('/'.preg_quote($type).'/', $this->browser->getLastResponse()->getHeader($header));
+	}
+
+	/**
+	 * Checks that response Header doesn't contains specific text.
+	 *
+	 * @param string $header
+	 *
+	 * @param string $type
+	 *
+	 * @Then /^(?:the )?response header "([^"]*)" should not contain "([^"]*)"$/
+	 */
+	public function theResponseHeaderShouldNotContain($header, $type)
+	{
+		assertNotRegExp('/'.preg_quote($type).'/', $this->browser->getLastResponse()->getHeader($header));
+	}
+
+	/**
      * Checks that response body contains JSON from PyString.
      *
      * @param PyStringNode $jsonString
