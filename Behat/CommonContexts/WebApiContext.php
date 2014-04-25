a2d926178de2780756bd4478ae21d547634e3760
Thomas Subera <ts@25th-floor.com>
2014-04-25 12:22:08 +0200

    fix typo

--- Behat/CommonContexts/WebApiContext.php
+++ Behat/CommonContexts/WebApiContext.php
@@ -228,7 +228,7 @@ class WebApiContext extends BehatContext
             assertArrayHasKey($key, $actual);
             $actualVal = $actual[$key];
             if (is_array($actualVal)) {
-                $this->assertContainsRecursive($expect[$key], $actualVal);
+                $this->assertArrayContainsRecursive($expect[$key], $actualVal);
             } else {
                 assertEquals($expect[$key], $actualVal);
             }
